4c586e0fc055399bd2dce812c77a4b5b16e7af28
Pallab Majumdar <kitkatpallab@gmail.com>
2023-12-24 11:37:00 +0600

    python function is done

---
+++ python_function.py
@@ -0,0 +1,10 @@
+
+def my_function(*kids):
+    """
+    The function `my_function` takes in a variable number of arguments representing children's names and
+    prints the name of the third youngest child.
+    """
+    print("The youngest child is " + kids[2])
+
+
+my_function("Emil", "Tobias", "Linus")
